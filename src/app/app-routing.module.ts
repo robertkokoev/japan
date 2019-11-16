bf3549be8a00a06af9d2a13abc29312e9ac033d0
robertkokoev <kokoev.robert@mail.ru>
2019-11-16 12:48:49 +0300

    Мелкие правки

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadingStrategy, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CultureComponent } from './components/culture/culture.component';
 import { HistoryComponent } from './components/history/history.component';
@@ -7,19 +7,16 @@ import { NewsComponent } from './components/news/news.component';
 import { VisitComponent } from './components/visit/visit.component';
 import { AboutComponent } from './components/about/about.component';
 
-const cultureChild: Routes = [
-  {path: 'history', component: HistoryComponent}
-];
-
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'culture', component: CultureComponent, children: cultureChild },
+  { path: 'culture', component: CultureComponent },
+  { path: 'culture/history', component: HistoryComponent },
   { path: 'news', component: NewsComponent },
   { path: 'visit', component: VisitComponent },
   { path: 'about', component: AboutComponent }
 ]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
